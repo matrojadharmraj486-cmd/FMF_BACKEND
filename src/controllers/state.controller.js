777fbc3e971df072157bd2bbcce0cf8f007ec330
matrojadharmraj486-cmd <matrojadharmraj486@used-on-github.com>
2025-10-24 00:06:31 +0530

    fix module error

--- src/controllers/state.controller.js
+++ src/controllers/state.controller.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const filePath = path.join(process.cwd(), 'src/data/state-districts.json');
 const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
