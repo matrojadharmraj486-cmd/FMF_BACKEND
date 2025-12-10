49c9cda7f8c970548872ceba48202c0a5dafab95
matrojadharmraj486-cmd <matrojadharmraj486@used-on-github.com>
2025-12-10 23:36:37 +0530

    change in model

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,32 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, sparse: true },
-  phone: { type: String, unique: true, sparse: true },
-  password: { type: String, required: true, select: false }
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: false },
+    phone: { type: String, unique: false },
+    password: { type: String, required: true, select: false }
+  },
+  { timestamps: true }
+);
 
-// Password hashing middleware
+// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // ✔ required
+  } catch (err) {
+    next(err); // ✔ pass error forward
+  }
 });
 
-// Password compare method
-userSchema.methods.comparePassword = async function (userPassword) {
-  return await bcrypt.compare(userPassword, this.password);
+// Compare password method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 export default mongoose.model("User", userSchema);
