85ab8dd130575770c92150bcb318a82354279721
matrojadharmraj486-cmd <matrojadharmraj486@used-on-github.com>
2025-12-10 22:32:49 +0530

    minor change

--- src/index..js
+++ src/index..js
@@ -1,5 +1,5 @@
-import app from './src/app.js';
-import connectDB from './src/config/database.js';
+import app from './app.js';
+import connectDB from './config/database.js';
 
 const PORT = process.env.PORT || 3000;
 
