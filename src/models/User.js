a7bf56b3b81a50a59beb0d4fc4d0979244480f13
matrojadharmraj486-cmd <matrojadharmraj486@used-on-github.com>
2025-12-11 21:46:51 +0530

    change in model

--- src/models/User.js
+++ src/models/User.js
@@ -12,17 +12,13 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next(); // ✔ required
-  } catch (err) {
-    next(err); // ✔ pass error forward
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
 
 // Compare password method
 userSchema.methods.comparePassword = async function (enteredPassword) {
