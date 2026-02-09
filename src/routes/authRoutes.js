6f3d9a5b673e06e0861e7691a802c16e46f2bd0b
matrojadharmraj486-cmd <matrojadharmraj486@gmail.com>
2026-02-09 22:09:30 +0530

    add route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { sendOtp, verifyOtp } from "../controller/otp.controller.js";
 import { login, register } from "../controller/authController.js";
+import { forgotPassword } from "../controller/forgotPasswordController.js";
+import { resetPassword } from "../controller/resetPassword.js";
 
 const router = express.Router();
 
@@ -8,5 +10,8 @@ router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router.post("/register", register);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+
 
 export default router;
