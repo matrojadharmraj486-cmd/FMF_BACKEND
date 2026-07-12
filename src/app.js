9850707c005d60f2026dbb4400c916071d285ecf
ronakrockers <divyang.rockersinfo@gmail.com>
2026-07-12 15:39:45 +0530

    add new URL in CORS

--- src/app.js
+++ src/app.js
@@ -73,7 +73,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:4173",
-  "http://127.0.0.1:4173"
+  "http://127.0.0.1:4173",
+  "https://admin.familymedicineflashback.in/"
 ];
 
 app.use(
